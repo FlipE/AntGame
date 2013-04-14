292d831ada32419430f9b123577d1ea53c045c36
Chris B <chris@cbaker.co.uk>
2013-04-14 18:34:25 +0100

    Fixed movement

--- AntGame/src/util/Position.java
+++ AntGame/src/util/Position.java
@@ -107,6 +107,7 @@ public class Position {
 		// 3 C 0
 		//  2 1
 		
+		boolean even = (this.y % 2 == 0);
 		int dx = 0;
 		int dy = 0;
 		
@@ -114,20 +115,22 @@ public class Position {
 			dx = 1;
 		}
 		else if(direction == 1) {
+			dx = even ? 0 : 1;
 			dy = 1;
 		}
 		else if(direction == 2) {
-			dx = -1;
+			dx = even ? -1 : 0;
 			dy = 1;	
 		}
 		else if(direction == 3) {
 			dx = -1;
 		}
 		else if(direction == 4) {
-			dx = -1;
+			dx = even ? -1 : 0;
 			dy = -1;
 		}
 		else if(direction == 5) {
+			dx = even ? 0 : 1;
 			dy = -1;
 		}
 		
